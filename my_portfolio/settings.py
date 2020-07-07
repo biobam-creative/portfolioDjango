8d2bbc02fa41618a17e24fb02e691be4fd78acd1
Emmanuel <eminentia.ea@gmail.com>
2020-07-07 04:03:31 -0700

    heroku ready

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -137,6 +137,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+prod_db = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
+
 # email Settings
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
